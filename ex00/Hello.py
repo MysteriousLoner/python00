56dc090fd3abeacf457edaa66f4bf533a3ee5820
MysteriousLoner <leeyang031110@gmail.com>
2023-09-27 22:00:36 +0800

    ex00

---
+++ ex00/Hello.py
@@ -0,0 +1,17 @@
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello": "titi!"}
+
+ft_list[1] = "World!"
+ft_temp_list = list(ft_tuple)
+ft_temp_list[1] = "Malaysia!"
+ft_tuple = tuple(ft_temp_list)
+ft_set.remove("tutu!")
+ft_set.add("Kuala Lumpur!")
+ft_dict['Hello'] = '42KL!'
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
